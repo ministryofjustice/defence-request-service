3515dbd4583890f9ee59320d69e7ecedd7d15ec4
Jack Bracewell <jack.bracewell@unboxedconsulting.com>
2015-02-02 15:54:16 +0000

    Change dashboard header, and hide actions based on role.

--- spec/features/user_authentication_feature_spec.rb
+++ spec/features/user_authentication_feature_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'user authentication' do
-  context 'with authentication' do
+  context  'cso with authentication' do
     let(:cso_password) { 'password' }
     let(:cso_user) { User.create(email: 'cso@example.com', password: cso_password, role: :cso) }
 
@@ -10,7 +10,7 @@ RSpec.feature 'user authentication' do
       fill_in 'user_email', with: cso_user.email
       fill_in 'user_password', with: cso_password
       click_button 'Sign in'
-      expect(page).to have_content('CSO DASHBOARD')
+      expect(page).to have_content('CSO Dashboard')
     end
 
     scenario 'logs in with invalid credentials' do
@@ -18,14 +18,35 @@ RSpec.feature 'user authentication' do
       fill_in 'user_email', with: cso_user.email
       fill_in 'user_password', with: 'notarealpassword'
       click_button 'Sign in'
-      expect(page).to_not have_content('CSO DASHBOARD')
+      expect(page).to_not have_content('CSO Dashboard')
+    end
+  end
+
+  context 'cco with authentication' do
+    let(:cco_password) { 'password' }
+    let(:cco_user) { User.create(email: 'cco@example.com', password: cco_password, role: :cco) }
+
+    scenario 'logs in with valid credentials' do
+      visit new_user_session_path
+      fill_in 'user_email', with: cco_user.email
+      fill_in 'user_password', with: cco_password
+      click_button 'Sign in'
+      expect(page).to have_content('CCO Dashboard')
     end
 
-    context 'unauthenticated' do
-      scenario 'logs in with valid credentials' do
-        visit root_path
-        expect(page).to have_button('Sign in')
-      end
+    scenario 'logs in with invalid credentials' do
+      visit new_user_session_path
+      fill_in 'user_email', with: cco_user.email
+      fill_in 'user_password', with: 'notarealpassword'
+      click_button 'Sign in'
+      expect(page).to_not have_content('CCO Dashboard')
+    end
+  end
+
+  context 'unauthenticated' do
+    scenario 'logs in with valid credentials' do
+      visit root_path
+      expect(page).to have_button('Sign in')
     end
   end
 end
