44ea025c568e813c37bbe7b2e794164e47aad616
Kips Davenport <patrick.davenport@unboxedconsulting.com>
2015-02-20 16:46:21 +0000

    Refactored policies

--- app/policies/defence_request_policy.rb
+++ app/policies/defence_request_policy.rb
@@ -19,23 +19,23 @@ class DefenceRequestPolicy < ApplicationPolicy
   end
 
   def index?
-    user.cso? || user.cco? || user.solicitor?
+    cso || cco || solicitor
   end
 
   def show?
-    user.cso? || user.cco? || user.solicitor?
+    cso || cco || solicitor
   end
 
   def new?
-    user.cso?
+    cso
   end
 
   def create?
-    user.cso?
+    cso
   end
 
   def edit?
-    (user.cso?) || (record.cco == user)
+    cso || (record.cco == user)
   end
 
   def update?
@@ -43,71 +43,93 @@ class DefenceRequestPolicy < ApplicationPolicy
   end
 
   def solicitors_search?
-    user.cso?
+    cso
   end
 
   def refresh_dashboard?
-    user.cso? || user.cco? || user.solicitor?
+    cso || cco || solicitor
   end
 
   def feedback?
-    (user.cso? || user.cco?) && record.can_transition?(:close)
+    (cso || cco) && can_transition?(:close)
   end
 
   def close?
-    (user.cso? || user.cco?) && record.can_transition?(:close)
+    (cso || cco) && can_transition?(:close)
   end
 
   def dscc_number_edit?
-    user.cco? && record.opened?
+    cco && record.opened?
   end
 
   def interview_start_time_edit?
-    user.cso? && !record.new_record? && record.created?
+    cso && !record.new_record? && record.created?
   end
 
   def case_details_edit?
-    user.cso? || (user.cco? && !record.created?)
+    cso || (cco && !record.created?)
   end
 
   def detainee_details_edit?
-    user.cso? || (user.cco? && !record.created?)
+    cso || (cco && !record.created?)
   end
 
   def solicitor_details_edit?
-   (user.cso? || user.cco?)
+   (cso || cco)
   end
 
   def open?
-    user.cco? && record.can_transition?(:open)
+    cco && can_transition?(:open)
   end
 
   def accept?
-    user.cco? && record.can_transition?(:accept) && record.dscc_number?
+    cco && can_transition?(:accept) && has_dscc_number?
   end
 
   def accepted?
-    user.cco? && record.can_transition?(:accept) && record.dscc_number?
+    cco && can_transition?(:accept) && has_dscc_number?
   end
 
   def view_open_requests?
-    user.cco? || user.cso?
+    cco || cso
   end
 
   def view_created_requests?
-    user.cco? || user.cso?
+    cco || cso
   end
 
   def view_accepted_requests?
-    user.cco? || user.cso? || user.solicitor?
+    cco || cso || solicitor
   end
 
   def resend_details?
-    (user.cco? || user.cso?) && record.accepted?
+    (cco || cso) && record.accepted?
   end
 
   def solicitor_time_of_arrival?
-    record.solicitor == user || (record.accepted? && (user.cco? || user.cso?))
+    record.solicitor == user || (record.accepted? && (cco || cso))
+  end
+
+  private
+
+  def can_transition?(state)
+    record.can_transition?(state)
+  end
+
+  def cso
+    user.cso?
+  end
+
+  def cco
+    user.cco?
+  end
+
+  def solicitor
+    user.solicitor?
+  end
+
+  def has_dscc_number?
+    record.dscc_number?
   end
 
 end
