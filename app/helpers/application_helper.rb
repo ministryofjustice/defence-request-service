12befdc1cbf9d77198c887c6b8ba2831efc5d999
Jack Bracewell <jack.bracewell@unboxedconsulting.com>
2015-02-25 14:25:39 +0000

    Delete some unused methods (shown up by simplecov)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,18 +15,6 @@ module ApplicationHelper
     params[:controller] + '/js_partials/' + params[:controller] + '_' + params[:action] + '_js.html.erb'
   end
 
-  def is_dashboard?
-    is_controller?('defence_requests') && is_action?('index')
-  end
-
-  def is_controller?(controller)
-    params[:controller] == controller
-  end
-
-  def is_action?(action)
-    params[:action] == action
-  end
-
   def date_formatter(date)
     date ? date.strftime("%F") : ''
   end
