fb244875457904daaf7d8c6ea9de901760834568
Jack Bracewell <jack.bracewell@unboxedconsulting.com>
2015-03-03 15:09:30 +0000

    Make string style, and descriptions consistent
    There are so many strings n the feature specs, the
    difference with single and double quotes was
    driving me mad.
    
    Given the new set of spec descriptions I think
    `specify` reads a lot better than `scenario`

--- spec/features/cco/defence_request_management_spec.rb
+++ spec/features/cco/defence_request_management_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.feature "call center operatives managing defence requests" do
   let(:cco_user) { FactoryGirl.create(:cco_user) }
@@ -10,7 +10,7 @@ RSpec.feature "call center operatives managing defence requests" do
   context "with any request" do
     let!(:accepted_dr) { create(:defence_request, :accepted) }
 
-    scenario "can see the show page of the request" do
+    specify "can see the show page of the request" do
       visit defence_requests_path
       within ".accepted-defence-request" do
         click_link "Show"
@@ -21,7 +21,7 @@ RSpec.feature "call center operatives managing defence requests" do
       expect(page).to have_link("Dashboard")
     end
 
-    scenario "can not edit expected time of arrival from the request show page" do
+    specify "can not edit expected time of arrival from the request show page" do
       visit defence_requests_path
       within ".accepted-defence-request" do
         click_link("Show")
@@ -30,22 +30,22 @@ RSpec.feature "call center operatives managing defence requests" do
       expect(page).to_not have_selector(".time-of-arrival")
     end
 
-    scenario 'can close the request from the dashboard' do
+    specify "can close the request from the dashboard" do
       visit root_path
       within "#defence_request_#{accepted_dr.id}" do
-        click_link 'Close'
+        click_link "Close"
       end
-      expect(page).to have_content('Close the Defence Request')
-      click_button 'Close'
+      expect(page).to have_content("Close the Defence Request")
+      click_button "Close"
       expect(page).to have_content("Feedback: can't be blank")
-      expect(page).to have_content('Close the Defence Request')
+      expect(page).to have_content("Close the Defence Request")
 
-      fill_in 'Feedback', with: 'I just cant take it any more...'
-      click_button 'Close'
-      expect(page).to have_content('Defence Request successfully closed')
-      expect(page).to have_content('Call Center Operative Dashboard')
+      fill_in "Feedback", with: "I just cant take it any more..."
+      click_button "Close"
+      expect(page).to have_content("Defence Request successfully closed")
+      expect(page).to have_content("Call Center Operative Dashboard")
       accepted_dr.reload
-      expect(accepted_dr.state).to eq 'closed'
+      expect(accepted_dr.state).to eq "closed"
     end
   end
 
@@ -54,7 +54,7 @@ RSpec.feature "call center operatives managing defence requests" do
     context "that have not yet been opened" do
       let!(:unopened_dr) { create(:own_solicitor) }
 
-      scenario "cannot edit the request" do
+      specify "cannot edit the request" do
         visit root_path
         within "#defence_request_#{unopened_dr.id}" do
           expect(page).not_to have_link("Edit")
@@ -62,7 +62,7 @@ RSpec.feature "call center operatives managing defence requests" do
       end
 
       #TODO - make better
-      scenario "can open the request" do
+      specify "can open the request" do
         visit root_path
         within "#defence_request_#{unopened_dr.id}" do
           click_button "Open"
@@ -70,7 +70,7 @@ RSpec.feature "call center operatives managing defence requests" do
       end
 
       #TODO - make better
-      scenario "cannot mark the request as accepted" do
+      specify "cannot mark the request as accepted" do
         visit root_path
         within ".created-defence-request" do
           expect(page).to_not have_button "Accepted"
@@ -84,7 +84,7 @@ RSpec.feature "call center operatives managing defence requests" do
     context "that have been opened by this cco" do
       let!(:opened_dr) { create(:defence_request, :opened, :with_solicitor, cco: cco_user) }
 
-      scenario "can edit the solicitors details on the request" do
+      specify "can edit the solicitors details on the request" do
         visit root_path
         within "#defence_request_#{opened_dr.id}" do
           click_link "Edit"
@@ -105,7 +105,7 @@ RSpec.feature "call center operatives managing defence requests" do
         end
       end
 
-      scenario "can add/edit the DSCC number on the request" do
+      specify "can add/edit the DSCC number on the request" do
         visit root_path
         within "#defence_request_#{opened_dr.id}" do
           click_link "Edit"
@@ -114,7 +114,7 @@ RSpec.feature "call center operatives managing defence requests" do
         click_button "Update Defence Request"
         expect(page).to have_content "Defence Request successfully updated"
         within "#defence_request_#{opened_dr.id}" do
-          click_link 'Edit'
+          click_link "Edit"
         end
         expect(page).to have_field "DSCC number", with: "NUMBERWANG"
         fill_in "DSCC number", with: "T-1000"
@@ -122,37 +122,37 @@ RSpec.feature "call center operatives managing defence requests" do
         expect(page).to have_content "Defence Request successfully updated"
       end
 
-      scenario "can mark the request as accepted from the request\'s edit page whilst adding a dscc number" do
+      specify "can mark the request as accepted from the request\"s edit page whilst adding a dscc number" do
         visit root_path
 
         within "#defence_request_#{opened_dr.id}" do
-          click_link 'Edit'
+          click_link "Edit"
         end
-        fill_in 'DSCC number', with: '123456'
+        fill_in "DSCC number", with: "123456"
 
-        click_button 'Update and Accept'
+        click_button "Update and Accept"
         within ".accepted-defence-request" do
           expect(page).to have_content(opened_dr.solicitor_name)
         end
       end
 
-      scenario 'can not choose "Update and Accept" from the request\'s edit page without adding a dscc number' do
+      specify "can not choose \"Update and Accept\" from the request's edit page without adding a dscc number" do
         visit root_path
 
         within "#defence_request_#{opened_dr.id}" do
-          click_link 'Edit'
+          click_link "Edit"
         end
 
-        click_button 'Update and Accept'
-        expect(page).to have_content('A Valid DSCC number is required to update and accept a Defence Request')
+        click_button "Update and Accept"
+        expect(page).to have_content("A Valid DSCC number is required to update and accept a Defence Request")
       end
 
-      context 'that have a dscc number' do
-        scenario 'can accept the request from the dashboard' do
-          opened_dr.update(dscc_number: '012345')
+      context "that have a dscc number" do
+        specify "can accept the request from the dashboard" do
+          opened_dr.update(dscc_number: "012345")
           visit root_path
           within "#defence_request_#{opened_dr.id}" do
-            click_button 'Accepted'
+            click_button "Accepted"
           end
           within ".accepted-defence-request" do
             expect(page).to have_content(opened_dr.solicitor_name)
@@ -160,44 +160,44 @@ RSpec.feature "call center operatives managing defence requests" do
         end
       end
 
-      context 'that have the "duty" solicitor type' do
+      context "that have the "duty" solicitor type" do
         let!(:duty_solicitor_dr) { create(:defence_request, :duty_solicitor, :opened, cco: User.first) }
 
-        scenario 'can not mark the request as accepted from the dashboard without solicitor detials' do
+        specify "can not mark the request as accepted from the dashboard without solicitor detials" do
           visit root_path
           within "#defence_request_#{duty_solicitor_dr.id}" do
-            expect(page).to_not have_button 'Accepted'
+            expect(page).to_not have_button "Accepted"
           end
         end
 
-        scenario 'can only mark the request as accepted from the edit page with solicitor details and dscc number' do
+        specify "can only mark the request as accepted from the edit page with solicitor details and dscc number" do
           visit root_path
 
           within "#defence_request_#{duty_solicitor_dr.id}" do
-            click_link 'Edit'
+            click_link "Edit"
           end
-          click_button 'Update and Accept'
-          expect(page).to have_content('Valid solicitor details are required to update and accept a Defence Request')
+          click_button "Update and Accept"
+          expect(page).to have_content("Valid solicitor details are required to update and accept a Defence Request")
 
-          within '.solicitor-details' do
-            fill_in 'Full Name', with: 'Dodgy Dave'
+          within ".solicitor-details" do
+            fill_in "Full Name", with: "Dodgy Dave"
           end
-          click_button 'Update and Accept'
-          expect(page).to have_content('Valid solicitor details are required to update and accept a Defence Request')
-          within '.solicitor-details' do
-            fill_in 'Full Name', with: 'Dodgy Dave'
-            fill_in 'Name of firm', with: 'Innocent your honour'
+          click_button "Update and Accept"
+          expect(page).to have_content("Valid solicitor details are required to update and accept a Defence Request")
+          within ".solicitor-details" do
+            fill_in "Full Name", with: "Dodgy Dave"
+            fill_in "Name of firm", with: "Innocent your honour"
           end
-          click_button 'Update and Accept'
+          click_button "Update and Accept"
 
-          expect(page).to have_content('A Valid DSCC number is required to update and accept a Defence Request')
-          within '.solicitor-details' do
-            fill_in 'Full Name', with: 'Dodgy Dave'
-            fill_in 'Name of firm', with: 'Innocent your honour'
+          expect(page).to have_content("A Valid DSCC number is required to update and accept a Defence Request")
+          within ".solicitor-details" do
+            fill_in "Full Name", with: "Dodgy Dave"
+            fill_in "Name of firm", with: "Innocent your honour"
           end
-          fill_in 'DSCC number', with: '123456'
-          click_button 'Update and Accept'
-          expect(page).to have_content('Defence Request successfully updated and marked as accepted')
+          fill_in "DSCC number", with: "123456"
+          click_button "Update and Accept"
+          expect(page).to have_content("Defence Request successfully updated and marked as accepted")
         end
       end
     end
@@ -207,7 +207,7 @@ RSpec.feature "call center operatives managing defence requests" do
   context "that have been marked as accepted" do
     let!(:accepted_dr) { create(:defence_request, :accepted, :with_solicitor, cco: cco_user) }
 
-    scenario 'can edit the expected time of arrival on the request' do
+    specify "can edit the expected time of arrival on the request" do
       visit root_path
 
       within ".accepted-defence-request" do
@@ -215,22 +215,31 @@ RSpec.feature "call center operatives managing defence requests" do
       end
 
       within ".accepted-defence-request" do
-        click_link 'Edit'
+        click_link "Edit"
       end
 
+<<<<<<< HEAD
       within '.solicitor-details' do
         fill_in 'defence_request_solicitor_time_of_arrival_day', with: '01'
         fill_in 'defence_request_solicitor_time_of_arrival_month', with: '01'
         fill_in 'defence_request_solicitor_time_of_arrival_year', with: '2001'
         fill_in 'defence_request_solicitor_time_of_arrival_hour', with: '01'
         fill_in 'defence_request_solicitor_time_of_arrival_min', with: '01'
+=======
+      within ".solicitor-details" do
+        select("2010", from: "defence_request_solicitor_time_of_arrival_1i")
+        select("January", from: "defence_request_solicitor_time_of_arrival_2i")
+        select("1", from: "defence_request_solicitor_time_of_arrival_3i")
+        select("12", from: "defence_request_solicitor_time_of_arrival_4i")
+        select("00", from: "defence_request_solicitor_time_of_arrival_5i")
+>>>>>>> Make string style, and descriptions consistent
       end
-      click_button 'Update Defence Request'
-      expect(page).to have_content 'Defence Request successfully updated'
+      click_button "Update Defence Request"
+      expect(page).to have_content "Defence Request successfully updated"
       within ".accepted-defence-request" do
-        click_link 'Show'
+        click_link "Show"
       end
-      expect(page).to have_content('1 January 2010 - 12:00')
+      expect(page).to have_content("1 January 2010 - 12:00")
     end
   end
 end
--- spec/features/solicitor/defence_request_management_spec.rb
+++ spec/features/solicitor/defence_request_management_spec.rb
@@ -11,7 +11,7 @@ RSpec.feature "solicitors managing defence requests" do
 
   context "with cases they are assigned to" do
 
-    scenario "they can see the show page" do
+    specify "can see the show page of the request" do
       visit defence_requests_path
       within ".accepted-defence-request" do
         click_link("Show")
@@ -22,7 +22,7 @@ RSpec.feature "solicitors managing defence requests" do
       expect(page).to have_link("Dashboard")
     end
 
-    scenario "they can edit the expected arrival time form the show page" do
+    specify "can edit the expected arrival time from the show page of the request" do
       visit defence_requests_path
       within ".accepted-defence-request" do
         click_link("Show")
@@ -47,7 +47,7 @@ RSpec.feature "solicitors managing defence requests" do
   end
 
   context "cases they are were assigned to, that are closed" do
-    scenario "they can see the feedback page and 'call Call Centre' message" do
+    specify "can see the feedback page and \"call Call Centre\" message" do
       visit defence_request_path(closed_dr)
       expect(page).to_not have_content("Case Details")
       expect(page).to_not have_content(closed_dr.solicitor_name)
@@ -58,8 +58,8 @@ RSpec.feature "solicitors managing defence requests" do
     end
   end
 
-  context  "with cases they are NOT assigned to" do
-    scenario "they can NOT see the show page" do
+  context "with cases they are not assigned to" do
+    specify  "can not see the show page" do
       expect{ visit defence_request_path(dr2) }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
