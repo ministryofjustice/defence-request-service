330bdcf49c06e234967d0f958fd7b03faa393347
Jack Bracewell <jack.bracewell@unboxedconsulting.com>
2015-03-09 00:20:11 +0000

    Use DateTimeField for timeofarrival on show page

--- app/controllers/defence_requests_controller.rb
+++ app/controllers/defence_requests_controller.rb
@@ -111,7 +111,8 @@ class DefenceRequestsController < BaseController
   end
 
   def solicitor_time_of_arrival
-    if @defence_request.update_attributes(defence_request_params)
+    new_time_of_arrival = DateTimeField.new defence_request_params['solicitor_time_of_arrival']
+    if @defence_request.update_attributes solicitor_time_of_arrival: new_time_of_arrival.value
       redirect_to(defence_request_path(@defence_request), notice: flash_message(:solicitor_time_of_arrival_added, DefenceRequest))
     else
       redirect_to(defence_request_path(@defence_request), alert: flash_message(:solicitor_time_of_arrival_not_added, DefenceRequest))
