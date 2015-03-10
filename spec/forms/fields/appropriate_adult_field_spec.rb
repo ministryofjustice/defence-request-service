702b52e5e4836fbce63c8473cb8c9e45b61b882a
Kips Davenport <patrick.davenport@unboxedconsulting.com>
2015-03-10 10:24:28 +0000

    Added working appropriate adult field object

---
+++ spec/forms/fields/appropriate_adult_field_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe AppropriateAdultField do
+
+  context "from persisted value" do
+    subject { AppropriateAdultField.from_persisted_value bool }
+    context "persisted value for true" do
+      let(:bool) { true }
+
+      it "has the same email as the original" do
+        expect(subject.appropriate_adult).to eql 'yes'
+      end
+    end
+
+    context "persisted value for false" do
+      let(:bool) { false }
+
+      it "has the same email as the original" do
+        expect(subject.appropriate_adult).to eql 'no'
+      end
+    end
+  end
+
+  context "from params" do
+    subject { AppropriateAdultField.new params }
+
+
+    context "with value yes" do
+      let(:params) { { appropriate_adult: 'yes' } }
+
+      it "has correct attribute" do
+        expect(subject.appropriate_adult).to eql 'yes'
+      end
+    end
+
+    context "with value no" do
+      let(:params) { { appropriate_adult: 'no' } }
+
+      it "has correct attribute" do
+        expect(subject.appropriate_adult).to eql 'no'
+      end
+    end
+  end
+
+  context "methods" do
+    context "value" do
+      context "for 'no'" do
+        subject { AppropriateAdultField.new appropriate_adult: 'no' }
+
+        it "returns 'no'" do
+          expect(subject.value).to eql false
+        end
+      end
+
+      context "for 'yes'" do
+        subject { AppropriateAdultField.new appropriate_adult: 'yes' }
+
+        it "returns 'yes'" do
+          expect(subject.value).to eql true
+        end
+      end
+    end
+  end
+end
