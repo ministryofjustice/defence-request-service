9325dffc8c6af3374ae0bff508584dac3f458002
Jack Bracewell <jack.bracewell@unboxedconsulting.com>
2015-03-25 16:25:01 +0000

    Style changes, and remove redundant `html_safe`s

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,23 +8,23 @@ module ApplicationHelper
     end
   end
 
-  def flash_message key, msg
+  def flash_message(key, msg)
     content_tag(:div, class: "#{key}-summary") do
       content_tag :p, msg
     end
   end
 
-  def object_error_messages active_model_messages
+  def object_error_messages(active_model_messages)
     content_tag(:ul) do
-      active_model_messages.map do |field_name, field_messages|
-        errors_for_field field_name, field_messages
-      end.join.html_safe
+      active_model_messages.each do |field_name, field_messages|
+        concat errors_for_field(field_name, field_messages)
+      end
     end
   end
 
   def errors_for_field field_name, field_messages
     content_tag :li do
-      "#{t(field_name)}: #{field_messages.join(', ').html_safe}".html_safe
+      "#{t(field_name)}: #{field_messages.join(', ')}".html_safe
     end
   end
 
