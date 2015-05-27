9e32eb4970b7d710fef0bdadaf7ae7a5f0836b88
Evangelos Giataganas <E.giataganas@gmail.com>
2015-05-27 17:31:26 +0300

    Create script.js
    This code is using inside the form to send the email

---
+++ js/script.js
@@ -0,0 +1,12 @@
+jQuery(function($) {
+  var form_action = $('#form_name').attr('action');
+    
+    $('#form_name').submit(function(ev) {
+      ev.preventDefault();
+      var subject = $('#email_subject').val(),
+        body = $('#email_body').val();
+      
+      window.location.href = form_action + '?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(body);
+      console.log(url);
+    });
+});
