3f49ce2ce2cf7bd208c263dab0b584d4292ec723
Jack Bracewell <jack.bracewell@unboxedconsulting.com>
2015-06-05 13:36:42 +0100

    Add "Day after tomorrow" to future date choosers

--- app/models/day.rb
+++ app/models/day.rb
@@ -10,4 +10,5 @@ class Day
   YESTERDAY = new "yesterday", Date.yesterday
   TODAY = new "today", Date.today
   TOMORROW = new "tomorrow", Date.tomorrow
+  DAY_AFTER_TOMORROW = new "day_after_tomorrow", Date.tomorrow + 1
 end
