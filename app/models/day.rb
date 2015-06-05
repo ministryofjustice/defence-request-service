18fd2f597217642b40c28b6fe50968c93c9b3e15
Jack Bracewell <jack.bracewell@unboxedconsulting.com>
2015-06-05 16:17:06 +0100

    Calculate Day values dynamically
    Previously Day::TODAY would just stick with the 
    same date that it had when the class was loaded.

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,14 +1,18 @@
 class Day
 
-  attr_reader :name, :date
+  attr_reader :name
 
-  def initialize(name, date)
+  def initialize(name, date_proc)
     @name = name
-    @date = date
+    @date_proc = date_proc
   end
 
-  YESTERDAY = new "yesterday", Date.yesterday
-  TODAY = new "today", Date.today
-  TOMORROW = new "tomorrow", Date.tomorrow
-  DAY_AFTER_TOMORROW = new "day_after_tomorrow", Date.tomorrow + 1
+  def date
+    @date_proc.call
+  end
+
+  YESTERDAY = new "yesterday", -> { Date.yesterday }
+  TODAY = new "today", -> { Date.today }
+  TOMORROW = new "tomorrow", -> { Date.tomorrow }
+  DAY_AFTER_TOMORROW = new "day_after_tomorrow", -> { Date.tomorrow + 1 }
 end
