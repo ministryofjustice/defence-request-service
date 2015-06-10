bb9e5e3e7a2a5983ba057137c97401d562a4485e
Clive Murray <clive.murray@digital.justice.gov.uk>
2015-06-10 13:49:52 +0100

    initial reorg for IE versions

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,9 +9,12 @@ Rails.application.config.assets.version = "1.0"
 # Precompile additional assets.
 # application.js, application.scss, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(
+      govuk_elements_extensions.css
       govuk_elements_extensions_ie6.css
       govuk_elements_extensions_ie7.css
       govuk_elements_extensions_ie8.css
+      ie6_overrides.css
+      ie7_overrides.css
       font-awesome-ie7.min.css
       dashboard_poller.js
     )
