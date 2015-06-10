19a1b90138b440c4e1b029af19cf0e1a6569f14f
Jakub Novotny <novotny.jakub@gmail.com>
2015-06-10 13:43:20 +0100

    Create feature test for interview time scenarios
    There are validation scenarios missing

--- spec/features/cso/changing_interview_time_spec.rb
+++ spec/features/cso/changing_interview_time_spec.rb
@@ -1,32 +1,77 @@
 require "rails_helper"
 
 RSpec.feature "Custody Suite Officers changing interview time for a defence request" do
-  # TODO: check new interview time it rendered when the below story is done:
-  # https://trello.com/c/RtWUhXVc/8-3-as-a-cso-i-d-like-to-add-the-interview-time-so-that-the-solicitor-knows-when-the-interview-starts
+  context "with javascript enabled", js: true do
+    specify "can set new interview time to an arbitrary date" do
+      create_and_display_defence_request
 
-  specify "can change interview time to an arbitrary date" do
-    login_create_and_submit_defence_request
+      within "div#interview" do
+        fill_in "Hour", with: "14"
+        fill_in "Min", with: "05"
+        fill_in "defence_request_interview_start_time_date", with: "4 June 2015"
+      end
 
-    # navigate to the defence request details page
-    find(".cso-defence-requests tbody tr:first-child td.actions a").click
+      click_button "Save interview time"
 
-    within "div#interview" do
-      fill_in "Hour", with: "14"
-      fill_in "Min", with: "05"
-      fill_in "defence_request_interview_start_time_date", with: "4 June 2015"
+      expect(page).to have_content("Interview at 14:05 4 June 2015")
     end
 
-    click_button "Save interview time"
+    specify "can change interview time to an arbitrary date" do
+      create_and_display_defence_request(true)
 
-    expect(page).to have_content("Interview start time updated")
+      within "div#interview" do
+        click_link "Change this"
+
+        fill_in "Hour", with: "16"
+        fill_in "Min", with: "05"
+        fill_in "defence_request_interview_start_time_date", with: "5 June 2015"
+      end
+
+      click_button "Save interview time"
+
+      expect(page).to have_content("Interview at 16:05 5 June 2015")
+    end
   end
 
-  def login_create_and_submit_defence_request
+  context "with javascript disabled" do
+    specify "can set new interview time to an arbitrary date" do
+      create_and_display_defence_request
+
+      within "div#interview" do
+        fill_in "Hour", with: "14"
+        fill_in "Min", with: "05"
+        fill_in "defence_request_interview_start_time_date", with: "4 June 2015"
+      end
+
+      click_button "Save interview time"
+
+      expect(page).to have_content("Interview at 14:05 4 June 2015")
+    end
+
+    specify "can change interview time to an arbitrary date" do
+      create_and_display_defence_request(true)
+
+      within "div#interview" do
+        click_link "Change this"
+      end
+
+      fill_in "Hour", with: "16"
+      fill_in "Min", with: "05"
+      fill_in "defence_request_interview_start_time_date", with: "5 June 2015"
+
+      click_button "Save interview time"
+
+      expect(page).to have_content("Interview at 16:05 5 June 2015")
+    end
+  end
+
+  def create_and_display_defence_request(interview_time_set = false)
+    traits = [ :acknowledged ]
+    traits << :interview_start_time if interview_time_set
+    dr = create :defence_request, *traits
+
     login_as_cso
 
-    click_link "New request"
-    fill_in_defence_request_form
-    click_button "Create request"
-    click_button "Submit request"
+    visit "/defence_requests/#{dr.id}"
   end
 end
